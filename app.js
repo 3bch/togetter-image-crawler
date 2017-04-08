c26118f4ae75a1ce4caa0364af2c4b4c5d9c29b5
3bch <t.mitsubuchi@gmail.com>
2017-04-08 21:22:16 +0900

    co から async/await に実装を切り替えた

--- app.js
+++ app.js
@@ -1,83 +1,96 @@
-const co = require('co');
-const axios = require('axios');
+'use strict';
+
 const fs = require('fs');
 const path = require('path');
+
+const axios = require('axios');
 const Nightmare = require('nightmare');
 
-const setTimeoutAsync = delay => new Promise(r => setTimeout(r, delay));
 
-const hasMoreButton = (nightmare) => {
-    return nightmare.exists('.more_tweet_box a.btn');
-};
+function delay(wait) {
+  return new Promise(resolve => setTimeout(resolve, wait));
+}
 
-const clickMoreButton = (nightmare) => {
+function hasMoreButton(nightmare) {
+  return nightmare.exists('.more_tweet_box a.btn');
+}
+
+function clickMoreButton(nightmare) {
   return nightmare
     .click('.more_tweet_box a.btn')
-    .wait('.pagenation')
-};
+    .wait('.pagenation');
+}
 
-const clickMoreButtonIfExist = (nightmare) => {
-  return co(function *() {
-    if(yield hasMoreButton(nightmare)) {
-      yield clickMoreButton(nightmare);
-    }
-  });
-};
+async function clickMoreButtonIfExist(nightmare) {
+  const existMore = await hasMoreButton(nightmare);
+  if (existMore) {
+    await clickMoreButton(nightmare);
+  }
+}
 
-const getTogetterImageUrls = (togetterUrl) => {
-  return co(function *() {
-    console.log(`fetching image urls... [${togetterUrl}]`);
-    const nightmare = Nightmare();
-    yield nightmare.goto(togetterUrl);
-    yield clickMoreButtonIfExist(nightmare);
-    const urls =
-      yield nightmare.evaluate(() => {
-        const nodeList = document.querySelectorAll('.list_photo img');
-        return Array.prototype.map.call(nodeList, (node) => node.src);
-      });
-    yield nightmare.end();
-    return urls;
-  });
-};
+async function getTogetterImageUrls(togetterUrl) {
+  console.log(`fetching image urls... [${togetterUrl}]`);
 
-const getImages = (urls, interval) => {
-  return co(function *() {
-    for(let i in urls) {
-      const url = urls[i];
-      const fileName = `images/${path.basename(url).split(':')[0]}`;
-      console.log(`${+i+1}/${urls.length} ${fileName}`);
-      const response = yield axios.get(url, {responseType: 'stream'});
-      response.data.pipe(fs.createWriteStream(fileName));
-      yield setTimeoutAsync(interval);
-    }
-  });
-};
+  const nightmare = Nightmare();
 
-const getTogetterImages = (togetterUrl, interval) => {
-  return co(function *() {
-    const urls = yield getTogetterImageUrls(togetterUrl);
-    return yield getImages(urls, interval);
-  });
-};
+  await nightmare.goto(togetterUrl);
+  await clickMoreButtonIfExist(nightmare);
 
-const getMaxPage = (togetterUrl) => {
-  return co(function *() {
-    const nightmare = Nightmare();
-    yield nightmare.goto(togetterUrl);
-    console.log(`fetching max page... [${togetterUrl}]`);
-    yield clickMoreButtonIfExist(nightmare);
-    const pageLinks =
-      yield nightmare.evaluate(() => {
-        const nodeList =  document.querySelectorAll('.pagenation a');
-        return Array.prototype.map.call(nodeList, (node) => node.textContent);
-      });
-    yield nightmare.end();
-    const pages = pageLinks
-      .filter(p => /^[0-9]+$/.test(p))
-      .map(Number);
-    return Math.max.apply(null, pages);
+  const urls = await nightmare.evaluate(() => {
+    const nodeList = document.querySelectorAll('.list_photo img');
+    return Array.prototype.map.call(nodeList, (node) => node.src);
   });
-};
+
+  await nightmare.end();
+  return urls;
+}
+
+async function getImages(urls, interval) {
+  for(let i in urls) {
+    const url = urls[i];
+    const fileName = `images/${path.basename(url).split(':')[0]}`;
+    console.log(`${+i+1}/${urls.length} ${fileName}`);
+    const response = await axios.get(url, {responseType: 'stream'});
+    response.data.pipe(fs.createWriteStream(fileName));
+    await delay(interval);
+  }
+}
+
+async function getTogetterImages(togetterUrl, interval) {
+  const urls = await getTogetterImageUrls(togetterUrl);
+  return await getImages(urls, interval);
+}
+
+async function getMaxPage(togetterUrl) {
+  const nightmare = Nightmare();
+
+  await nightmare.goto(togetterUrl);
+
+  console.log(`fetching max page... [${togetterUrl}]`);
+  await clickMoreButtonIfExist(nightmare);
+
+  const pageLinks = await nightmare.evaluate(() => {
+    const nodeList = document.querySelectorAll('.pagenation a');
+    return Array.prototype.map.call(nodeList, (node) => node.textContent);
+  });
+
+  await nightmare.end();
+
+  const pages = pageLinks
+    .filter(link => /^[0-9]+$/.test(link))
+    .map(Number);
+
+  return Math.max.apply(null, pages);
+}
+
+async function main(togetterUrl, interval) {
+  const maxPage = await getMaxPage(togetterUrl);
+  for(let i = 0; i < maxPage; i++) {
+    console.log(`page ${i+1}/${maxPage}`);
+    await getTogetterImages(togetterUrl + '?page=' + (i+1), interval);
+  }
+}
+
 
 const togetterUrl = process.argv[2];
 let interval = process.argv[3] || 2000;
@@ -89,11 +102,10 @@ if(!togetterUrl.includes('http')) {
   process.exit(1);
 }
 
-co(function *() {
-  const maxPage = yield getMaxPage(togetterUrl);
-  for(let i = 0; i < maxPage; i++) {
-    console.log(`page ${i+1}/${maxPage}`);
-    yield getTogetterImages(togetterUrl + '?page=' + (i+1), interval);
-  }
-})
-  .then(() => console.log('finished!'), e => console.log(e));
+
+main(togetterUrl, interval).then(() => {
+  console.log('finished!');
+}).catch(e => {
+  console.log(e);
+  process.exit(1);
+});
